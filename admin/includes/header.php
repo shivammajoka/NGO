d8e10e1a4f1dec2aaaa4b801975c61df62117fa1
shivam <shivammajoka1988@gmail.com>
2024-01-30 18:08:46 +0530

    add by shivam

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -3,14 +3,14 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>Xeloro - Admin & Dashboard Template</title>
+        <title>Sewabharti</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="MyraStudio" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
         <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <link rel="shortcut icon" href="assets/images/logosewa.png">
 
         <!-- App css -->
         <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
