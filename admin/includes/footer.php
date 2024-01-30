9360fd7873bc98fc94ee61004b10caf4d9bb04a6
shivam <shivammajoka1988@gmail.com>
2024-01-30 17:26:39 +0530

    admin add by shivam

---
+++ admin/includes/footer.php
@@ -0,0 +1,54 @@
+<footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="text-center text-lg-left">
+                                2020 © Xeloro.
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="text-right d-none d-lg-block">
+                                Design & Develop by Myra
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+        <!-- end main content-->
+
+    </div>
+    <!-- END layout-wrapper -->
+
+
+    <!-- jQuery  -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/waves.js"></script>
+    <script src="assets/js/simplebar.min.js"></script>
+
+    <!-- Sparkline Js-->
+    <script src="../plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+
+    <!-- Chart Js-->
+    <script src="../plugins/jquery-knob/jquery.knob.min.js"></script>
+
+    <!-- Chart Custom Js-->
+    <script src="assets/pages/knob-chart-demo.js"></script>
+
+    <!-- Morris Js-->
+    <script src="../plugins/morris-js/morris.min.js"></script>
+
+    <!-- Raphael Js-->
+    <script src="../plugins/raphael/raphael.min.js"></script>
+
+    <!-- Custom Js -->
+    <script src="assets/pages/dashboard-demo.js"></script>
+
+    <!-- App js -->
+    <script src="assets/js/theme.js"></script>
+
+</body>
+
+</html>
