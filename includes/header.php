7fd1ce8edcb6fbba09974cb184e21f07e0180fd9
Aalsi <thorprimeold01@outlook.com>
2024-01-30 19:25:03 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -11,6 +11,7 @@
    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
    <link rel="stylesheet" href="assets/css/slick.css">
+   <link rel="stylesheet" href="assets/css/stylelocal.css">
    <!-- Font Awesome 6 -->
    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
    <link rel="stylesheet" href="assets/css/slick-theme.css">
@@ -23,7 +24,10 @@
    <!-- color -->
    <link rel="stylesheet" href="assets/css/color.css">
    <link rel="stylesheet" href="assets/css/newscroll.css">
+
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    <style>
         #news-container {
