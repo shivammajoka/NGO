378ba49f91fdef59858720b0f0edfac62c9cc4e7
Aalsi <thorprimeold01@outlook.com>
2024-01-31 17:51:58 +0530

    added new pages
    edit by sk

---
+++ भोजन वितरण केन्द्र.php
@@ -0,0 +1,195 @@
+<?php include('includes/header.php') ?>
+<section class="page-title-area" style="background-image:url(assets/img/slider-1.png)">
+  <div class="container">
+    <div class="title-area-data">
+      <h2>भोजन वितरण केन्द्र</h2>
+      <p>Organisations committed to ending poverty worldwide.</p>
+    </div>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item">
+        <a href="index.html">Home</a>
+      </li>
+      <li class="breadcrumb-item active" aria-current="page"> Our Event</li>
+        <li class="breadcrumb-item active" aria-current="page">Event Details</li>
+    </ol>
+  </div>
+</section>
+<section class="gap">
+  <div class="container">
+    <div class="event-details hoverimg">
+      <figure>
+        <img alt="img" src="assets/img/slider-1.png">
+      </figure>
+      <div class="event-details-text p-100">
+        <div class="upcoming-event-time">
+            <h4>27<span>Jul<br>2023</span></h4>
+            <div class="upcoming-event-separator"></div>
+            <h4>30<span>sep<br>2023</span></h4>
+          </div>
+        <h2>Trustee Leadership Programme – London Event Program Spring 2023</h2>
+        <div class="row pt-4 pb-4 align-items-center">
+          <div class="col-lg-4 col-md-6">
+            <div class="event-time">
+              <i class="fa-solid fa-calendar-days"></i>
+              <div>
+                <span>Opening Times</span>
+                <h6>Mon - Thu 9am - 5pm</h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="event-time">
+              <i class="fa-solid fa-location-dot"></i>
+              <div>
+                <span>Event Location</span>
+                <h6>London Park Uk</h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="event-time mb-0">
+              <i class="fa-solid fa-ticket"></i>
+              <div>
+                <span>Tickets Information:</span>
+                <h6>Free</h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="organizer">
+             <img alt="man" src="https://via.placeholder.com/90x90">
+              <div>
+                <h4>Walkar Thomas</h4>
+                <h6>Event Organizer,  Director NGO</h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="organizer">
+             <img alt="man" src="https://via.placeholder.com/90x90">
+              <div>
+                <h4>Willimes Domson</h4>
+                <h6>Charity Speacker</h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-12">
+            <p class="pt-4 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis au 
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat a
+            oident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><br>
+
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ni si ut aliquip ex eu fugiat nulla pa
+            cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="col-lg-5">
+            <div class="event-img-two ">
+              <figure>
+                <img alt="img" src="https://via.placeholder.com/440x460">
+              </figure>
+            </div>
+          </div>
+          <div class="col-lg-7 ps-sm-5">
+            <p class="pb-4">Duis sed odio sit amet nibh vulputate cursus a amet ma san ip
+                veli Nam nec tellus a odio tincidunt auct ursus mauris Morbi ac
+                msan ipsuy veli Nam nec per tora tor ec tellus. cursus a amet m
+                 accumsan ipsuy veli Nam cumsan ipsuy veli Nam nec perus.  a
+                Nam nec per tora tor ec tellus. cursus </p>
+            <ul class="team-list">
+            <li><i class="fa-regular fa-circle-check"></i>Watch the Bathroom Sessions</li>
+            <li><i class="fa-regular fa-circle-check"></i>Find everything you need to get fundraising from the comfort </li>
+            <li><i class="fa-regular fa-circle-check"></i>Order a personalised present</li>
+            <li><i class="fa-regular fa-circle-check"></i>What have you given today.</li>
+            </ul>
+          </div>
+        </div>
+        <div class="youtube-video mt-5 mb-5 hoverimg">
+            <a data-fancybox="" href="https://www.youtube.com/watch?v=1La4QzGeaaQ"><i>
+              <svg enable-background="new 0 0 437.499 437.499" height="512" viewBox="0 0 437.499 437.499" width="512" xmlns="http://www.w3.org/2000/svg"><path d="m46.875 437.498c-2.67 0-5.341-.687-7.751-2.06-4.868-2.777-7.874-7.95-7.874-13.566v-406.27c0-5.616 3.006-10.789 7.874-13.566 4.913-2.762 10.88-2.701 15.701.107l343.749 203.136c4.761 2.823 7.675 7.935 7.675 13.459s-2.914 10.636-7.675 13.459l-343.749 203.135c-2.457 1.435-5.204 2.167-7.95 2.166zm15.625-394.521v351.521l297.409-175.76z"></path></svg>
+            </i></a>
+            <figure>
+              <img alt="img" src="https://via.placeholder.com/1070x470">
+            </figure>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis au
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat a
+          oident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div class="obviously">
+          <h3>There is obviously a lot of uncertainty but that doesn’t have to be a scary thing, as the great Yogi Berra said, </h3>
+        </div>
+        <div class="row pt-4">
+          <div class="col-lg-4 col-md-4 col-sm-6 hoverimg">
+            <div class="event-details-img">
+              <figure>
+                <img alt="img" class="w-100" src="https://via.placeholder.com/349x365">
+              </figure>
+            </div>
+            <div class="event-details-img">
+              <figure>
+                <img alt="img" class="w-100" src="https://via.placeholder.com/349x251">
+              </figure>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-6">
+            <div class="event-details-img">
+              <figure>
+                <img alt="img" class="w-100" src="https://via.placeholder.com/349x251">
+              </figure>
+            </div>
+            <div class="event-details-img">
+              <figure>
+                <img alt="img" class="w-100" src="https://via.placeholder.com/349x365">
+              </figure>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="event-details-img">
+              <figure>
+                <img alt="img" class="w-100" src="https://via.placeholder.com/349x251">
+              </figure>
+            </div>
+            <div class="event-details-img">
+              <figure>
+                <img alt="img" class="w-100" src="https://via.placeholder.com/349x365">
+              </figure>
+            </div>
+          </div>
+        </div>
+        <p class="pt-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aut
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat a
+          oident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><br>
+
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ni si ut aliquip ex eu fugiat nulla pa
+          cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div class="contact-map pt-4">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387190.27988761215!2d-74.25986708594112!3d40.69767006828117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2s!4v1670920528913!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        <p class="pt-4">
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aut dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat a oident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div class="share-post-icon mt-5">
+            <h6>share post:</h6>
+            <ul class="social-media-icon full">
+                <li>
+                  <a href="#">
+                    <i class="fab fa-facebook-f icon"></i>    </a>
+                </li>
+                <li>
+                  <a href="#"><i class="fab fa-twitter icon"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fab fa-google-plus-g icon"></i></a></li>
+            </ul>
+          </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include('includes/footer.php') ?>
