c68b23c3dbfe176d08e6699974137037abe76ec6
Aalsi <thorprimeold01@outlook.com>
2024-01-31 17:10:32 +0530

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -21,37 +21,106 @@ else{
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sewabharti</title>
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
-    <link rel="shortcut icon" href="assets/images/logosewa.png">
-    <link rel="stylesheet" href="assets/css/login2.css">
-</head>
-<body >
-    <div class="wrapper">
-        <form action="" method="POST">
-            <h1>Login</h1>
-            <div class="input-box">
-                <input type="text" placeholder="user" name="user" required>
-                <i class='bx bxs-user'></i>
-            </div>
-            <div class="input-box">
-                <input type="password" placeholder="pass" name="pass" required>
-                <i class='bx bxs-lock-alt' ></i>
-            </div>
-            <div class="remember-forgot">
-                <label><input type="checkbox">Remember me</label>
-                <a href="#">Forgot password?</a>
-            </div>
 
-            <button type="submit" class="btn" name="submit">Login</button>
+    <head>
+        <meta charset="utf-8" />
+        <title>sewabharti</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="MyraStudio" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-            <div class="register-link">
-                <p>Don't have an account? <a href="#">Regeister</a></p>
+        <!-- App favicon -->
+        <link rel="icon" href="assets/images/logosewa.png">
+
+        <!-- App css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body>
+ 
+        <div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="d-flex align-items-center min-vh-100">
+                            <div class="w-100 d-block bg-white shadow-lg rounded my-5">
+                                <div class="row">
+                                    <!-- <div class="col-lg-5 d-none d-lg-block bg-login rounded-left"></div> -->
+                                    <div class="col-lg-5">
+                                        <img src="assets/images/logosewa.png" alt="">
+                                    </div>
+                                    <div class="col-lg-7">
+                                        <div class="p-5">
+                                            <div class="text-center mb-5">
+                                                <a href="index.html" class="text-dark font-size-22 font-family-secondary">
+                                                    <b class="align-middle">सेवा भारती मध्य भारत</b>
+                                                </a>
+                                            </div>
+                                            <h1 class="h5 mb-1">Welcome Back!</h1>
+                                            <p class="text-muted mb-4">Enter your email address and password to access admin panel.</p>
+                                            <form class="user" method="POST">
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address" name="user">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" name="pass">
+                                                </div>
+                                                <!-- <a href="" class="btn btn-success btn-block waves-effect waves-light" type="submit" name="submit"> Log In </a> -->
+                                                <button type="submit" class="btn btn-success btn-block waves-effect waves-light" name="submit">login</button>
+                                                <div class="text-center mt-4">
+                                                    <h5 class="text-muted font-size-16">Sign in using</h5>
+                                                
+                                                    <!-- <ul class="list-inline mt-3 mb-0">
+                                                        <li class="list-inline-item">
+                                                            <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>
+                                                        </li>
+                                                        <li class="list-inline-item">
+                                                            <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>
+                                                        </li>
+                                                        <li class="list-inline-item">
+                                                            <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
+                                                        </li>
+                                                        <li class="list-inline-item">
+                                                            <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github-circle"></i></a>
+                                                        </li>
+                                                    </ul> -->
+                                                </div>
+                                                
+                                            </form>
+    
+                                            <div class="row mt-4">
+                                                <div class="col-12 text-center">
+                                                    <p class="text-muted mb-2"><a href="pages-recoverpw.html" class="text-muted font-weight-medium ml-1">Forgot your password?</a></p>
+                                                    <p class="text-muted mb-0">Don't have an account? <a href="pages-register.html" class="text-muted font-weight-medium ml-1"><b>Sign Up</b></a></p>
+                                                </div> <!-- end col -->
+                                            </div>
+                                            <!-- end row -->
+                                        </div> <!-- end .padding-5 -->
+                                    </div> <!-- end col -->
+                                </div> <!-- end row -->
+                            </div> <!-- end .w-100 -->
+                        </div> <!-- end .d-flex -->
+                    </div> <!-- end col-->
+                </div> <!-- end row -->
             </div>
-        </form>
-</body>
-</html>
+            <!-- end container -->
+        </div>
+        <!-- end page -->
+    
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/metismenu.min.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/simplebar.min.js"></script>
+    
+        <!-- App js -->
+        <script src="assets/js/theme.js"></script>
+    
+    </body>
+    
+    </html>
